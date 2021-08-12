545e0a76ca7c7266ef6e0bd8d6e412b424b4bba8
izzy <yzziizzy@gmail.com>
2021-08-12 16:10:04 -0500

    add linking command add .gitignore

--- build.c
+++ build.c
@@ -606,7 +606,7 @@ FAIL:
 	return 0;
 }
 
-void check_source(char* raw_src_path) {
+void check_source(char* raw_src_path, strlist* objs) {
 	time_t src_mtime, obj_mtime = 0, dep_mtime = 0;
 	
 	char* src_path = resolve_path(raw_src_path, &src_mtime);
@@ -619,7 +619,10 @@ void check_source(char* raw_src_path) {
 	// cheap and dirty
 	size_t olen = strlen(obj_path);
 	obj_path[olen-1] = 'o';
-		
+	
+	
+	strlist_push(objs, obj_path);
+	
 	char* dep_path = strcatdup(build_dir, "/", base, ".d");
 	
 	mkdirp_cached(build_dir, 0755);
@@ -631,19 +634,13 @@ void check_source(char* raw_src_path) {
 		return;
 	}
 	
-//	char* real_dep_path = resolve_path(dep_path, &dep_mtime);
-//	if(dep_mtime >= src_mtime) {
-//		printf("  deptime compile\n");
-//		compile_source(src_path, real_obj_path);
-//		return;
-//	}
-	
-	
 	
 	if(gen_deps(src_path, dep_path, src_mtime, obj_mtime)) {
 		printf("  deep dep compile\n");
 		compile_source(src_path, real_obj_path);
 	}
+	
+	
 	//gcc -c -o $2 $1 $CFLAGS $LDADD
 }
 
@@ -655,21 +652,33 @@ int main(int argc, char* argv[]) {
 	strlist_init(&compile_cache);
 	hash_init(&mkdir_cache, 128);
 	
+	char* exe_path = "build/imcalc";
+	
 	
 	mkdirp_cached("build", 0755);
 	
 	g_gcc_opts_list = concat_lists(ld_add, cflags);
 	g_gcc_opts_flat = join_str_list(g_gcc_opts_list, " ");
 	
-	rglob src;
+//	rglob src;
 	//recursive_glob("src", "*.[ch]", 0, &src);
+	
+	strlist objs;
+	strlist_init(&objs);
 	
 	for(int i = 0; sources[i]; i++) {
 		printf("%i: checking %s\n", i, sources[i]);
-		check_source(sources[i]);
+		check_source(sources[i], &objs);
 	}
 	
 	compile_cache_execute();
+	
+	
+	char* objects_flat = join_str_list(objs.entries, " ");
+//	gcc -o imcalc $objlist $CFLAGS $LDADD
+	char* cmd = sprintfdup("gcc -o %s %s %s", exe_path, objects_flat, g_gcc_opts_flat);
+	system(cmd);
+	
 	
 //	printf("%d: %s\n", err, strerror(errno));
 	return 0;
